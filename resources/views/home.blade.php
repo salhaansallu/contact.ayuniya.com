ad7f01321b42a77f9f39ae6657abc058b94b6abc
Salhaan Sallu <salhaansoft@gmail.com>
2023-09-05 01:04:37 +0530

    bug fixed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Send us a message</title>
-    @vite(['resources/scss/app.scss','resources/js/app.js'])
+    @vite(['resources/sass/app.scss','resources/js/app.js'])
 </head>
 <body>
     <div class="site-wrap">
